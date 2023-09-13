fec018b80a65a79e3292a4b97a3d2e0676a2b87e
Peter <petermaturwe@gmail.com>
2023-09-13 13:37:11 -0700

    printing functions

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+void _islower(void);
+void _isalpha(void);
+void print_sign(void);
+void print_last_digit(void);
+
+#endif /* MAIN_H */
+
