dd08d0190ffb8ebd4d7ba365289ef32455b1e36a
Peter <petermaturwe@gmail.com>
2023-09-15 12:56:22 -0700

    compiled question 3

---
+++ 0x04-more_functions_nested_loops/3-main.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    print_numbers();  /* Call the print_numbers function */
+    return (0);
+}
