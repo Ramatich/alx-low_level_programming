9eeb77b6a9dfb0b40d4324ba5ca1f34efe01cc86
Peter <petermaturwe@gmail.com>
2023-09-22 13:55:36 -0700

    question 2 solved

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,3 +1,13 @@
+#include "main.h"
+
+/**
+ * _strncpy - Copy at most n characters from src to dest
+ * @dest: Destination string
+ * @src: Source string
+ * @n: Maximum number of characters to copy
+ *
+ * Return: Pointer to dest
+ */
 char *_strncpy(char *dest, char *src, int n)
 {
     int i;
