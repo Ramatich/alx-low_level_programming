73b6d87529677a9bde3a8918e405fc3b5c4c923d
Peter <petermaturwe@gmail.com>
2023-09-21 20:11:26 -0700

    added question 2

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -8,25 +8,25 @@
  *
  * Return: A pointer to the resulting string dest
  */
-char *_strncat(char *dest, const char *src, int n)
+char *_strncat(char *dest, char *src, int n)
 {
     int dest_len = 0;
     int i = 0;
 
-    // Find the length of the dest string
+    /* Find the length of the dest string */
     while (dest[dest_len] != '\0')
     {
         dest_len++;
     }
 
-    // Append characters from src to dest
+    /* Append characters from src to dest */
     while (i < n && src[i] != '\0')
     {
         dest[dest_len + i] = src[i];
         i++;
     }
 
-    // Null-terminate the resulting string
+    /* Null-terminate the resulting string */
     dest[dest_len + i] = '\0';
 
     return dest;
