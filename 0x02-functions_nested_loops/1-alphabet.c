49e76078ff8ae705ff0ab233641e58e9b5b0253f
Ramatich <petermaturwe@gmail.com>
2023-10-10 11:31:43 -0700

    used betty style in my code

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,15 @@
 #include "main.h"
-
 /**
- * print_alphabet - prints the lowercase alphabet
- */
+*print_alphabet - function that print alphabet
+*it prints in lowercase
+*Return: no return
+*/
 void print_alphabet(void)
 {
-    char letter;
-
-    for (letter = 'a'; letter <= 'z'; letter++)
-        _putchar(letter);
-    _putchar('\n');
+int letter;
+for (letter = 'a'; letter <= 'z'; letter++)
+{
+_putchar (letter);
 }
-
+_putchar (10);
+}
