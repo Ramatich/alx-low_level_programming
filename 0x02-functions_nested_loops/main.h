68078bf08eb81999b787b440031dfd50753a9565
Ramatich <petermaturwe@gmail.com>
2023-10-10 21:40:17 -0700

    compiled the program

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -5,6 +5,7 @@ int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _isalpha(int c);
+int _abs(int n);
 void print_last_digit(void);
 /* Function prototype for the add function */
 int add(int a, int b);
