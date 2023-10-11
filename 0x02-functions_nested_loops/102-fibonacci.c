ec5144792c5c448a5e2af13423b65dc9a78ba69a
Ramatich <petermaturwe@gmail.com>
2023-10-11 01:56:08 -0700

    used betty style in the program

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,26 +1,21 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
- *
  * Return: Always 0
  */
 int main(void)
 {
-    long int fib1 = 1, fib2 = 2, next;
-    int i;
-
-    printf("%ld, %ld", fib1, fib2);
-
-    for (i = 2; i < 50; i++)
-    {
-        next = fib1 + fib2;
-        printf(", %ld", next);
-        fib1 = fib2;
-        fib2 = next;
-    }
-
-    printf("\n");
-    return (0);
+long int fib1 = 1, fib2 = 2, next;
+int i;
+printf("%ld, %ld", fib1, fib2);
+for (i = 2; i < 50; i++)
+{
+next = fib1 + fib2;
+printf(", %ld", next);
+fib1 = fib2;
+fib2 = next;
+}
+printf("\n");
+return (0);
 }
 
