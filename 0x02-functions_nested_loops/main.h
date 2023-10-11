72f81c0405a95d3044944a37eaf94d124c54a556
Ramatich <petermaturwe@gmail.com>
2023-10-11 01:34:53 -0700

    compiling the 100 times table

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -10,7 +10,7 @@ int print_last_digit(int n);
 /* Function prototype for the add function */
 int add(int a, int b);
 void print_to_98(int n);
-void times_table(void);
+void print_times_table(int n);
 int print_sign(int n);
 /* Function prototype for the _islower function */
 int _islower(int c);
