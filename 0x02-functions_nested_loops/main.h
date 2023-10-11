2a7ec7d0f47245d904ee4d2763d0d05476dafb55
Ramatich <petermaturwe@gmail.com>
2023-10-10 21:49:12 -0700

    completed the last digit

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -6,7 +6,7 @@ void print_alphabet(void);
 void print_alphabet_x10(void);
 int _isalpha(int c);
 int _abs(int n);
-void print_last_digit(void);
+int print_last_digit(int n);
 /* Function prototype for the add function */
 int add(int a, int b);
 void print_to_98(int n);
