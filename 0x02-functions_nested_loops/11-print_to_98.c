02ac4b09c5757b3f2b7960ff7296958e6118580c
Ramatich <petermaturwe@gmail.com>
2023-10-11 01:12:33 -0700

    added the betty style to print

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,30 +1,25 @@
+#include "main.h"
 #include <stdio.h>
-
 /**
- * print_to_98 - Prints all natural numbers from n to 98.
- * @n: The starting number.
- *
- * Return: void (no return value).
- */
+*print_to_98 - prints natural
+*numbers from n to 98
+*@n:integer to start by
+*return: returns nothing
+*/
 void print_to_98(int n)
 {
-    int i;
+if (n >= 98)
+{
+while (n > 98)
+printf("%d, ", n--);
 
-    if (n <= 98) {
-        for (i = n; i <= 98; i++) {
-            printf("%d", i);
-            if (i < 98) {
-                printf(", ");
-            }
-        }
-    } else {
-        for (i = n; i >= 98; i--) {
-            printf("%d", i);
-            if (i > 98) {
-                printf(", ");
-            }
-        }
-    }
-    printf("\n");
+printf("%d\n", n);
 }
+else
+{
+while (n < 98)
+printf("%d, ", n++);
 
+printf("%d\n", n);
+}
+}
