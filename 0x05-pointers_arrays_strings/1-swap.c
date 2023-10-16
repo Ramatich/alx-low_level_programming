af577b75177801039a79dec0008e9f67f186e180
Ramatich <petermaturwe@gmail.com>
2023-10-16 11:36:18 -0700

    added betty style and documentation

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -3,13 +3,14 @@
  * swapping values of two integers
  * 
  * @a: An integer to swap
- * @b:another integer to swipe
- *
+ * @b:another integer to swip
+ * This function takes two integer pointers, 'a' and 'b', and swaps their values.
  * Return void
  */
 void swap_int(int *a, int *b)
 {
-int aux;
+int aux0;
+i
 aux = *a;
 *a = *b;
 *b = aux;
