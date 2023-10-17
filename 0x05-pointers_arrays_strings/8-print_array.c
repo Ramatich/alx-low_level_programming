bc7d0bdaa38bdc1dfdff21e701f16ea3c36d7bb0
Ramatich <petermaturwe@gmail.com>
2023-10-16 18:26:13 -0700

    worked on question 9

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,28 +1,22 @@
-#include "main.h"
-#include "stdio.h"
+#include "holberton.h"
+#include <stdio.h>
 /**
- * rev_string - This fuction print a string reverser
- *
- *@s: this is the pointer that point to a string
- */
-void rev_string(char *s)
+  * print_array - Prints n elements of an array of integers
+  * @a: Array of integers
+  * @n: Number of elements of the array to be printed
+  *
+  * Return: void
+  */
+void print_array(int *a, int n)
 {
-int a, len;
-char *begin, *end = s;
-for (a = 0; s[a] != '\0' && s[a + 1] != '\0'; a++)
+int j;
+for (j = 0; j < n; j++)
 {
-end++;
+printf("%d", a[j]);
+if (j != (n - 1))
+{
+printf(", ");
 }
-len = a + 1;
-begin = s;
-for (a = 0; a < len / 2; a++)
-{
-char x;
-x = *end;
-*end = *begin;
-*begin = x;
-begin++;
-end--;
 }
-end[len + 1] = '\0';
+printf("\n");
 }
