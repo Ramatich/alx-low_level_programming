686297e1dbecd553a2fc7f5f6442c6b62257a394
Ramatich <petermaturwe@gmail.com>
2023-10-19 11:35:37 -0700

    added betty style

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * _strncat - Concatenates two strings using at most n bytes from src
  * @dest: The destination string
@@ -10,25 +9,21 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-    int dest_len = 0;
-    int i = 0;
-
-    /* Find the length of the dest string */
-    while (dest[dest_len] != '\0')
-    {
-        dest_len++;
-    }
-
-    /* Append characters from src to dest */
-    while (i < n && src[i] != '\0')
-    {
-        dest[dest_len + i] = src[i];
-        i++;
-    }
-
-    /* Null-terminate the resulting string */
-    dest[dest_len + i] = '\0';
-
-    return dest;
+int dest_len = 0;
+int i = 0;
+/* Find the length of the dest string */
+while (dest[dest_len] != '\0')
+{
+dest_len++;
+}
+/* Append characters from src to dest */
+while (i < n && src[i] != '\0')
+{
+dest[dest_len + i] = src[i];
+i++;
+}
+/* Null-terminate the resulting string */
+dest[dest_len + i] = '\0';
+return dest;
 }
 
